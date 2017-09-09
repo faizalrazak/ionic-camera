dbcc667d547edc49a87e23b54a8abf7813e1ef2c
faizalrazak <mohamadfaizalrazak@gmail.com>
2017-09-09 14:19:21 +0800

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+image:any;
+options: CameraOptions = {
+	 quality: 100,
+	 destinationType: this.camera.DestinationType.DATA_URL,
+	 encodingType: this.camera.EncodingType.JPEG,
+	 mediaType: this.camera.MediaType.PICTURE
+	}
+
+  constructor(public navCtrl: NavController, private camera:Camera) {
 
   }
 
+takePhoto(){
+	this.camera.getPicture(this.options).then((imageData) => {
+	 // imageData is either a base64 encoded string or a file URI
+	 // If it's base64:
+	 let base64Image = 'data:image/jpeg;base64,' + imageData;
+	 this.image = base64Image;
+	}, (err) => {
+	 // Handle error
+	});
+  }
+
+  sharePhoto(){
+
+  }
 }
